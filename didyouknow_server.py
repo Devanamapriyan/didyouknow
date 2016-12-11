5af970a3960ec4c399c59e5001c5e5510768d238
Devanamapriyan <joeljose420@gmail.com>
2016-12-11 20:08:09 +0530

    second commit

--- didyouknow_server.py
+++ didyouknow_server.py
@@ -2,56 +2,42 @@
 
 import urllib2
 import json
-from flask import Flask, request
-from flask_restful import Resource, Api
-
-app = Flask(__name__)
-api = Api(app)
-
-todos = {}
-
-class TodoSimple(Resource):
-    def get(self, todo_id):
-        return {todo_id: todos[todo_id]}
-
-    def delete(self, todo_id):
-        todos[todo_id] = ""
-        return {todo_id: todos[todo_id]}
-
-    def put(self, todo_id):
-        todos[todo_id] = request.form['data']
-        return {todo_id: todos[todo_id]}
-
-api.add_resource(TodoSimple, '/<string:todo_id>')
-
-if __name__ == '__main__':
-    app.run(debug=True)
-    
-
-##topic = "docker_(disambiguation)"
-###topic = "Docker_(software)"
+##from flask import Flask, request
+##from flask_restful import Resource, Api
 ##
-##content = urllib2.urlopen("https://en.wikipedia.org/w/api.php?action=query&titles="+topic+"&prop=extracts&explaintext&format=json").read()
-###print content
+##app = Flask(__name__)
+##api = Api(app)
 ##
+##todos = {}
 ##
-##result = json.loads(content)
+##class TodoSimple(Resource):
+##    def get(self, todo_id):
+##        return {todo_id: todos[todo_id]}
 ##
-##key = result['query']['pages'].keys()
-##key = key[0]
+##    def delete(self, todo_id):
+##        todos[todo_id] = ""
+##        return {todo_id: todos[todo_id]}
 ##
-##print topic + "\n"
-##print result['query']['pages'][key]['extract']
+##    def put(self, todo_id):
+##        todos[todo_id] = request.form['data']
+##        return {todo_id: todos[todo_id]}
+##
+##api.add_resource(TodoSimple, '/<string:todo_id>')
+##
+##if __name__ == '__main__':
+##    app.run(debug=True)
 
 
 
+# to do : DB API for storing the topic name, wikipedia data and current location pointer.
+# for now, lets simulate for a single case without db.
 
 
-def get_Correct_Topic () :
-    topic = "docker_(disambiguation)"
+def get_Correct_Topic (topic) :
+    wikiname = topic + "_(disambiguation)"
     #topic = "Docker_(software)"
 
-    content = urllib2.urlopen("https://en.wikipedia.org/w/api.php?action=query&titles="+topic+"&prop=extracts&explaintext&format=json").read()
+    content = urllib2.urlopen("https://en.wikipedia.org/w/api.php?action=query&titles="+wikiname+"&prop=extracts&explaintext&format=json").read()
     #print content
 
 
@@ -60,7 +46,9 @@ def get_Correct_Topic () :
     key = result['query']['pages'].keys()
     key = key[0]
 
-    print topic + "\n"
+    print wikiname + "\n"
     print result['query']['pages'][key]['extract']
 
 
+
+get_Correct_Topic ("docker")
