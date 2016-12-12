4e5b1699059f2f2d0e91a25872f997f6fc4f1729
Devanamapriyan <joeljose420@gmail.com>
2016-12-12 15:34:35 +0530

    sixth commit

--- didyouknow_server.py
+++ didyouknow_server.py
@@ -9,11 +9,12 @@ from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
-topic_list = {"hello" : ["World"]}
+topic_list = {"user" : ["topicname"]}
+content = {"user": {"topicname" : ["content"]}}
 
 class nextTopic(Resource):
-     def get(self, topic_id):
-         return {topic_id: topic_list[topic_id]}
+     def get(self, username, topic_id):
+         return {topic_id: topic_list[username][topic_id]}
 
 class listAllTopic(Resource):
     def get(self):
@@ -25,13 +26,17 @@ class listUserTopic(Resource):
     
 class delTopic(Resource):
     def delete(self, username, topic_id):
+        username = request.form['username']
+        topic_id = request.form['topic_id']
         del(topic_list[username][topic_id])
         return {topic_list[username]}
     
 class addTopic(Resource):
-    def put(self, topic_name):
-        topic_list[topic_counter++] = request.form['name']
-        wikiname = topic + "_(disambiguation)"
+    def put(self, username, topic_name):
+        username = request.form['username']
+        topic_name = request.form['topic_name']
+        (topic_list[username]).append(topic_name)
+        wikiname = topic_name + "_(disambiguation)"
         content = urllib2.urlopen("https://en.wikipedia.org/w/api.php?action=query&titles="+wikiname+"&prop=extracts&explaintext&format=json").read()
         result = json.loads(content)
         key = result['query']['pages'].keys()
@@ -39,9 +44,10 @@ class addTopic(Resource):
         if (key == "-1"):
             print "Error with the topic. No such topic found on wikipedia!!"
             return -1
-        print "***************************************\n" + wikiname + "\n"
-        print result['query']['pages'][key]['extract'] 
-        return {todo_id: todos[todo_id]}
+        result = "***************************************\n" + wikiname + "\n"
+        result.append(result['query']['pages'][key]['extract'])
+        (content[username][topic_name]).append(result)
+        return {topic_list[username]}
 
 api.add_resource(TodoSimple, '/<string:todo_id>')
 if __name__ == '__main__':
